7c74fb326519ae14a0a5d37d05c2caf4f3081657
tom3murray <TKM2UFT@VIRGINIA.EDU>
2022-12-07 16:47:18 -0500

    Small edits to questions

--- Parts1-2.py
+++ Parts1-2.py
@@ -5,7 +5,7 @@
     # [Best Movies Netflix]
 # 3: How many shows have a ranking above X 
     # [Best Shows Netflix]
-# 4: What were the top x shows on Netflix in year Y 
+# 4: What were the top X shows on Netflix in year Y 
     # [Best Shows Netflix]
 # 5: What was the most common genre among the best movie rankings in year Y 
     # [Best Movies Netflix]
@@ -16,6 +16,6 @@
 # 8: What is the average number of seasons for shows released in year X
     # [raw_titles]
 # 9: What genre was the top Netflix show in year X
-    # [Best Show by Year]
+    # [Best Shows Netflix]
 # 10: How many movies released in year X were rated "R"
     # [raw_titles]
